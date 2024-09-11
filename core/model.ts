21f36779a0c028fe4b18dd027952ff540c3b9736
Mhmad <mohammadharb96170@gmail.com>
2024-09-11 16:29:49 +0300

    fix

--- core/model.ts
+++ core/model.ts
@@ -1,25 +1,25 @@
 import { POSITION, isStr, isNum, isFn, Type } from "../utils";
 import { eventManager, type OnChangeCallback, Event } from "./eventManager";
 import {
-  type DialogOptions,
-  type ToastProps,
-  type Id,
-  type UpdateOptions,
-  type ClearWaitingQueueParams,
-  type NotValidatedToastProps,
-  type TypeOptions,
-  type DialogContent,
+    type DialogOptions,
+    type ToastProps,
+    type Id,
+    type UpdateOptions,
+    type ClearWaitingQueueParams,
+    type NotValidatedToastProps,
+    type TypeOptions,
+    type DialogContent,
 } from "../types";
 import { type ContainerInstance } from "../hooks";
 import {
-  type MessageDialogProps,
-  WithMessageToast,
+    type MessageDialogProps,
+    WithMessageToast,
 } from "../components/WithMessageToast";
 import { type ButtonProps, Zoom } from "@mui/material";
 
 interface EnqueuedToast {
-  content: DialogContent<any>;
-  options: NotValidatedToastProps;
+    content: DialogContent<any>;
+    options: NotValidatedToastProps;
 }
 
 let containers = new Map<ContainerInstance | Id, ContainerInstance>();
@@ -31,24 +31,24 @@ let TOAST_ID = 1;
  * Get the toast by id, given it's in the DOM, otherwise returns null
  */
 function getToast(toastId: Id, { containerId }: DialogOptions) {
-  const container = containers.get(containerId || latestInstance);
-  return container && container.getToast(toastId);
+    const container = containers.get(containerId || latestInstance);
+    return container && container.getToast(toastId);
 }
 
 /**
  * Generate a random toastId
  */
 function generateToastId() {
-  return `${TOAST_ID++}`;
+    return `${TOAST_ID++}`;
 }
 
 /**
  * Generate a toastId or use the one provided
  */
 function getToastId(options?: DialogOptions) {
-  return options && (isStr(options.toastId) || isNum(options.toastId))
-    ? options.toastId
-    : generateToastId();
+    return options && (isStr(options.toastId) || isNum(options.toastId))
+        ? options.toastId
+        : generateToastId();
 }
 
 /**
@@ -56,179 +56,179 @@ function getToastId(options?: DialogOptions) {
  * the container lazy mounted
  */
 function dispatchToast<TData>(
-  content: DialogContent<TData>,
-  options: NotValidatedToastProps,
+    content: DialogContent<TData>,
+    options: NotValidatedToastProps
 ): Id {
-  if (containers.size > 0) {
-    eventManager.emit(Event.Show, content, options);
-  } else {
-    queue.push({ content, options });
-  }
+    if (containers.size > 0) {
+        eventManager.emit(Event.Show, content, options);
+    } else {
+        queue.push({ content, options });
+    }
 
-  return options.toastId;
+    return options.toastId;
 }
 
 /**
  * Merge provided options with the defaults settings and generate the toastId
  */
 function mergeOptions(type: string, options?: DialogOptions) {
-  return {
-    ...options,
-    type: (options && options.type) || type,
-    toastId: getToastId(options),
-  } as NotValidatedToastProps;
+    return {
+        ...options,
+        type: (options && options.type) || type,
+        toastId: getToastId(options),
+    } as NotValidatedToastProps;
 }
 
-function createToastByType<TData = MessageDialogProps>(type: string) {
-  return <TData = MessageDialogProps>(
-    content: DialogContent<TData>,
-    options?: DialogOptions,
-  ) => dispatchToast(WithMessageToast(content), mergeOptions(type, options));
+function createToastByType(type: string) {
+    return <TData = MessageDialogProps>(
+        content: DialogContent<TData>,
+        options?: DialogOptions
+    ) => dispatchToast(WithMessageToast(content), mergeOptions(type, options));
 }
 
 function model<TData = unknown>(
-  content: DialogContent<TData>, // data message
-  options?: DialogOptions, // toast
+    content: DialogContent<TData>, // data message
+    options?: DialogOptions // toast
 ) {
-  return dispatchToast(content, mergeOptions(Type.DEFAULT, options));
+    return dispatchToast(content, mergeOptions(Type.DEFAULT, options));
 }
 
 function modelMessage<TData = unknown>(
-  content: DialogContent<TData>, // data message
-  options?: DialogOptions, // toast
+    content: DialogContent<TData>, // data message
+    options?: DialogOptions // toast
 ) {
-  return dispatchToast(
-    WithMessageToast(content),
-    mergeOptions(Type.DEFAULT, options),
-  );
+    return dispatchToast(
+        WithMessageToast(content),
+        mergeOptions(Type.DEFAULT, options)
+    );
 }
 
 model.loading = <TData = unknown>(
-  content: DialogContent<TData>,
-  options?: DialogOptions,
+    content: DialogContent<TData>,
+    options?: DialogOptions
 ) =>
-  dispatchToast(
-    content,
-    mergeOptions(Type.DEFAULT, {
-      autoClose: false,
-      closeOnClick: false,
-      closeButton: false,
-      ...options,
-    }),
-  );
+    dispatchToast(
+        content,
+        mergeOptions(Type.DEFAULT, {
+            autoClose: false,
+            closeOnClick: false,
+            closeButton: false,
+            ...options,
+        })
+    );
 
 export interface ToastPromiseParams<
-  TData = unknown,
-  TError = unknown,
-  TPending = unknown,
+    TData = unknown,
+    TError = unknown,
+    TPending = unknown
 > {
-  pending?: string | UpdateOptions<TPending>;
-  success?: string | UpdateOptions<TData>;
-  error?: string | UpdateOptions<TError>;
+    pending?: string | UpdateOptions<TPending>;
+    success?: string | UpdateOptions<TData>;
+    error?: string | UpdateOptions<TError>;
 }
 
 function handlePromise<TData = unknown, TError = unknown, TPending = unknown>(
-  promise: Promise<TData> | (() => Promise<TData>),
-  { pending, error, success }: ToastPromiseParams<TData, TError, TPending>,
-  options?: DialogOptions,
+    promise: Promise<TData> | (() => Promise<TData>),
+    { pending, error, success }: ToastPromiseParams<TData, TError, TPending>,
+    options?: DialogOptions
 ) {
-  let id: Id;
+    let id: Id;
 
-  if (pending) {
-    id = isStr(pending)
-      ? model.loading(pending, options)
-      : model.loading(pending.render, {
-          ...options,
-          ...(pending as DialogOptions),
-        });
-  }
-
-  const resetParams = {
-    autoClose: null,
-    closeOnClick: null,
-    closeButton: null,
-    delay: 100,
-  };
-
-  const resolver = <T>(
-    type: TypeOptions,
-    input: string | UpdateOptions<T> | undefined,
-    result: T,
-  ) => {
-    // Remove the toast if the input has not been provided. This prevents the toast from hanging
-    // in the pending state if a success/error toast has not been provided.
-    if (input == null) {
-      model.dismiss(id);
-      return;
+    if (pending) {
+        id = isStr(pending)
+            ? model.loading(pending, options)
+            : model.loading(pending.render, {
+                  ...options,
+                  ...(pending as DialogOptions),
+              });
     }
 
-    const baseParams = {
-      type,
-      ...resetParams,
-      ...options,
-      data: result,
+    const resetParams = {
+        autoClose: null,
+        closeOnClick: null,
+        closeButton: null,
+        delay: 100,
     };
-    const params = isStr(input) ? { render: input } : input;
 
-    // if the id is set we know that it's an update
-    if (id) {
-      model.update(id, {
-        ...baseParams,
-        ...params,
-      } as UpdateOptions);
-    } else {
-      // using toast.promise without loading
-      model(params!.render, {
-        ...baseParams,
-        ...params,
-      } as DialogOptions);
-    }
+    const resolver = <T>(
+        type: TypeOptions,
+        input: string | UpdateOptions<T> | undefined,
+        result: T
+    ) => {
+        // Remove the toast if the input has not been provided. This prevents the toast from hanging
+        // in the pending state if a success/error toast has not been provided.
+        if (input == null) {
+            model.dismiss(id);
+            return;
+        }
 
-    return result;
-  };
+        const baseParams = {
+            type,
+            ...resetParams,
+            ...options,
+            data: result,
+        };
+        const params = isStr(input) ? { render: input } : input;
 
-  const p = isFn(promise) ? promise() : promise;
+        // if the id is set we know that it's an update
+        if (id) {
+            model.update(id, {
+                ...baseParams,
+                ...params,
+            } as UpdateOptions);
+        } else {
+            // using toast.promise without loading
+            model(params!.render, {
+                ...baseParams,
+                ...params,
+            } as DialogOptions);
+        }
 
-  //call the resolvers only when needed
-  p.then((result) => resolver("success", success, result)).catch((err) =>
-    resolver("error", error, err),
-  );
+        return result;
+    };
 
-  return p;
+    const p = isFn(promise) ? promise() : promise;
+
+    //call the resolvers only when needed
+    p.then((result) => resolver("success", success, result)).catch((err) =>
+        resolver("error", error, err)
+    );
+
+    return p;
 }
 
 async function Confirm(message: string) {
-  return new Promise((resolve) => {
-    model.warn(message, {
-      data: {
-        Title: "Confirm",
-        onResolved() {
-          resolve(true);
-        },
-        onReject() {
-          resolve(false);
-        },
-        ActionComponentProps: {
-          rejectTitle: "Cancel",
-          resolvedTitle: "Ok",
-          RejectButtonProps: {
-            variant: "contained",
-            color: "error",
-          } as ButtonProps,
-          ResolvedButtonProps: {
-            variant: "contained",
-            color: "primary",
-          } as ButtonProps,
-        },
-      },
-      disableClose: true,
-      transition: Zoom,
-      DialogProps: {
-        maxWidth: "xs",
-        fullWidth: true,
-      },
+    return new Promise((resolve) => {
+        model.warn(message, {
+            data: {
+                Title: "Confirm",
+                onResolved() {
+                    resolve(true);
+                },
+                onReject() {
+                    resolve(false);
+                },
+                ActionComponentProps: {
+                    rejectTitle: "Cancel",
+                    resolvedTitle: "Ok",
+                    RejectButtonProps: {
+                        variant: "contained",
+                        color: "error",
+                    } as ButtonProps,
+                    ResolvedButtonProps: {
+                        variant: "contained",
+                        color: "primary",
+                    } as ButtonProps,
+                },
+            },
+            disableClose: true,
+            transition: Zoom,
+            DialogProps: {
+                maxWidth: "xs",
+                fullWidth: true,
+            },
+        });
     });
-  });
 }
 
 model.promise = handlePromise;
@@ -240,72 +240,72 @@ model.warn = model.warning;
 model.modelMessage = modelMessage;
 model.confirm = Confirm;
 model.dark = (content: DialogContent, options?: DialogOptions) =>
-  dispatchToast(
-    content,
-    mergeOptions(Type.DEFAULT, {
-      theme: "dark",
-      ...options,
-    }),
-  );
+    dispatchToast(
+        content,
+        mergeOptions(Type.DEFAULT, {
+            theme: "dark",
+            ...options,
+        })
+    );
 
 /**
  * Remove toast programmaticaly
  */
 model.dismiss = (id?: Id) => {
-  if (containers.size > 0) {
-    eventManager.emit(Event.Clear, id);
-  } else {
-    queue = queue.filter((t) => id != null && t.options.toastId !== id);
-  }
+    if (containers.size > 0) {
+        eventManager.emit(Event.Clear, id);
+    } else {
+        queue = queue.filter((t) => id != null && t.options.toastId !== id);
+    }
 };
 
 /**
  * Clear waiting queue when limit is used
  */
 model.clearWaitingQueue = (params: ClearWaitingQueueParams = {}) =>
-  eventManager.emit(Event.ClearWaitingQueue, params);
+    eventManager.emit(Event.ClearWaitingQueue, params);
 
 /**
  * return true if one container is displaying the toast
  */
 model.isActive = (id: Id) => {
-  let isToastActive = false;
+    let isToastActive = false;
 
-  containers.forEach((container) => {
-    if (container.isToastActive && container.isToastActive(id)) {
-      isToastActive = true;
-    }
-  });
+    containers.forEach((container) => {
+        if (container.isToastActive && container.isToastActive(id)) {
+            isToastActive = true;
+        }
+    });
 
-  return isToastActive;
+    return isToastActive;
 };
 
 model.update = <TData = unknown>(
-  toastId: Id,
-  options: UpdateOptions<TData> = {},
+    toastId: Id,
+    options: UpdateOptions<TData> = {}
 ) => {
-  // if you call toast and toast.update directly nothing will be displayed
-  // this is why I defered the update
-  setTimeout(() => {
-    const toast = getToast(toastId, options as DialogOptions);
-    if (toast) {
-      const { props: oldOptions, content: oldContent } = toast;
+    // if you call toast and toast.update directly nothing will be displayed
+    // this is why I defered the update
+    setTimeout(() => {
+        const toast = getToast(toastId, options as DialogOptions);
+        if (toast) {
+            const { props: oldOptions, content: oldContent } = toast;
 
-      const nextOptions = {
-        ...oldOptions,
-        ...options,
-        toastId: options.toastId || toastId,
-        updateId: generateToastId(),
-      } as ToastProps & UpdateOptions;
+            const nextOptions = {
+                ...oldOptions,
+                ...options,
+                toastId: options.toastId || toastId,
+                updateId: generateToastId(),
+            } as ToastProps & UpdateOptions;
 
-      if (nextOptions.toastId !== toastId) nextOptions.staleId = toastId;
+            if (nextOptions.toastId !== toastId) nextOptions.staleId = toastId;
 
-      const content = nextOptions.render || oldContent;
-      delete nextOptions.render;
+            const content = nextOptions.render || oldContent;
+            delete nextOptions.render;
 
-      dispatchToast(content, nextOptions);
-    }
-  }, 0);
+            dispatchToast(content, nextOptions);
+        }
+    }, 0);
 };
 
 /**
@@ -329,10 +329,10 @@ model.update = <TData = unknown>(
  * ```
  */
 model.onChange = (callback: OnChangeCallback) => {
-  eventManager.on(Event.Change, callback);
-  return () => {
-    eventManager.off(Event.Change, callback);
-  };
+    eventManager.on(Event.Change, callback);
+    return () => {
+        eventManager.off(Event.Change, callback);
+    };
 };
 
 model.POSITION = POSITION;
@@ -342,25 +342,25 @@ model.POSITION = POSITION;
  * and attach isActive method
  */
 eventManager
-  .on(Event.DidMount, (containerInstance: ContainerInstance) => {
-    latestInstance = containerInstance.containerId || containerInstance;
-    containers.set(latestInstance, containerInstance);
+    .on(Event.DidMount, (containerInstance: ContainerInstance) => {
+        latestInstance = containerInstance.containerId || containerInstance;
+        containers.set(latestInstance, containerInstance);
 
-    queue.forEach((item) => {
-      eventManager.emit(Event.Show, item.content, item.options);
+        queue.forEach((item) => {
+            eventManager.emit(Event.Show, item.content, item.options);
+        });
+
+        queue = [];
+    })
+    .on(Event.WillUnmount, (containerInstance: ContainerInstance) => {
+        containers.delete(containerInstance.containerId || containerInstance);
+
+        if (containers.size === 0) {
+            eventManager
+                .off(Event.Show)
+                .off(Event.Clear)
+                .off(Event.ClearWaitingQueue);
+        }
     });
-
-    queue = [];
-  })
-  .on(Event.WillUnmount, (containerInstance: ContainerInstance) => {
-    containers.delete(containerInstance.containerId || containerInstance);
-
-    if (containers.size === 0) {
-      eventManager
-        .off(Event.Show)
-        .off(Event.Clear)
-        .off(Event.ClearWaitingQueue);
-    }
-  });
 
 export { model };
